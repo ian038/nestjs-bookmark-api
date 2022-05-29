c0d34ff0bbc653a8437d2c3562a8d0d7a4163dc6
ian038 <ianphua3@gmail.com>
2022-05-29 17:42:11 -0400

    Added nestjs jwt auth strategy

---
+++ src/auth/strategy/strategy.ts
@@ -0,0 +1,20 @@
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { PrismaService } from "src/prisma/prisma.service";
+
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+    constructor(config: ConfigService, private prisma: PrismaService) {
+        super({ jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), secretOrKey: config.get('JWT_Secret') })
+    }
+
+    async validate(payload: { sub: number, email: string }) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: payload.sub
+            }
+        })
+        delete user.hash
+        return user
+    }
+}

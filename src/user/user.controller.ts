ca28c467f439e8d26b7252ec81d38c0102d4bf97
ian038 <ianphua3@gmail.com>
2022-05-31 13:55:57 -0400

    Implementing e2e testing

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { JwtGuard } from 'src/auth/guard';
+import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
 
 @Controller('users')
 export class UserController {
     @UseGuards(JwtGuard)
     @Get('me')
-    getMe() {
-        return 'user info'
+    getMe(@GetUser('') user: User) {
+        return user
+    }
+
+    @Patch()
+    editUser() {
+
     }
 }
